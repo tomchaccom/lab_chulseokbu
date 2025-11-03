cc879db88b8a4deede87d01dac1826d14cc5c6f5
tomchccom <dreamkms2014@gmail.com>
2025-11-03 14:09:17 +0900

    Initial commit: backend project source

---
+++ backend/src/main/java/com/example/LabAttendance/RollCall/RollCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.LabAttendance.RollCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollCallApplication.class, args);
+	}
+
+}
