acc55b90c579bcb7330c23bb47ff7107dc6963e1
김정우 <read2437@naver.com>
2025-11-06 00:36:01 +0900

    Member 회원가입 컨트롤러 기본 틀

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberController.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberController.java
@@ -4,13 +4,10 @@ package com.example.LabAttendance.RollCall.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // Http 처리 요청
+@RestController
 @RequiredArgsConstructor
 public class MemberController
 {
     private final MemberService memberService;
-
-    //매핑 하기
-
 
 }
