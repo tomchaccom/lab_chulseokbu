d65f0b4ab9271b990ee04966a02c58ea722bafa0
김정우 <read2437@naver.com>
2025-11-06 00:36:17 +0900

    Member 회원가입 리포지터리 기본 틀

---
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.LabAttendance.RollCall.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member,Long>
+{
+
+}
+
