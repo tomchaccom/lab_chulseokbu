fc1b7ba96fe0d6bf8aa00c269c1f9f1387179ea9
김정우 <read2437@naver.com>
2025-11-08 19:34:07 +0900

    Member 엔티티 Setter 추가

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/Member.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/Member.java
@@ -6,12 +6,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
