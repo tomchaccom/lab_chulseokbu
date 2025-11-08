d9884f87fd8223140b9ffb19599f4dc65d5161a0
김정우 <read2437@naver.com>
2025-11-08 19:16:29 +0900

    Member 서비스 구현
    비밀번호 해시 적용 후 DB에 저장하게 구현.

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
@@ -2,12 +2,34 @@ package com.example.LabAttendance.RollCall.Member;
 
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class MemberService
 {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Long create(Member member)
+    {
+
+        if (memberRepository.existsByEmail(member.getEmail())) {
+            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        }
+
+
+        String hashedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(hashedPassword);
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
 
 
 
