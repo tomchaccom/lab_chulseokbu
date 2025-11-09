6eac9ebf567585a1e5b749c6bfa10b0c99ebd85a
김정우 <read2437@naver.com>
2025-11-09 16:38:31 +0900

    Member 회원가입 서비스 수정
    DTO를 이용한 이메일 중복 검사
    비밀번호에 해시를 적용
    DB에 저장되면 프론트에 ResponseDTO 값 반환
    
    password, email, phone 변경 미구현

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
@@ -1,10 +1,11 @@
 package com.example.LabAttendance.RollCall.Member;
 
+import com.example.LabAttendance.RollCall.Member.DTO.MemberResponseDto;
+import com.example.LabAttendance.RollCall.Member.DTO.MemberSignupRequestDto;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.annotation.Validated;
 
 @RequiredArgsConstructor
 @Service
@@ -14,23 +15,34 @@ public class MemberService
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public Long create(Member member)
+    public MemberResponseDto create(MemberSignupRequestDto requestDto)
     {
 
-        if (memberRepository.existsByEmail(member.getEmail())) {
-            throw new IllegalStateException("이미 존재하는 이메일입니다.");
+        if (memberRepository.existsByEmail(requestDto.email())) {
+            throw new DuplicateEmailException("이미 존재하는 이메일입니다: " + requestDto.email());
         }
 
+        String rawPassword = requestDto.password();
+        String hashedPassword = passwordEncoder.encode(rawPassword);
 
-        String hashedPassword = passwordEncoder.encode(member.getPassword());
-        member.setPassword(hashedPassword);
+
+        Member member = Member.builder()
+                .memberId(requestDto.memberId())
+                .nickname(requestDto.nickname())
+                .password(hashedPassword)
+                .email(requestDto.email())
+                .phone(requestDto.phone())
+                .gender(requestDto.gender())
+                .build();
 
         memberRepository.save(member);
 
-        return member.getId();
+        return new MemberResponseDto(
+                member.getId(),     // Long 타입 ID
+                member.getMemberId(),
+                member.getNickname(),
+                member.getEmail()
+        );
     }
-
-
-
 
 }
