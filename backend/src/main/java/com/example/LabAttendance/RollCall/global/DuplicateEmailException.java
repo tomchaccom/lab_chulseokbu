6890d13f1d5c0b93f19a9cfa7f8fccd9ccef33e2
김정우 <read2437@naver.com>
2025-11-12 01:55:09 +0900

    DuplicateEmailException 예외 처리 함수 이동

--- backend/src/main/java/com/example/LabAttendance/RollCall/global/DuplicateEmailException.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/global/DuplicateEmailException.java
@@ -1,6 +1,7 @@
 package com.example.LabAttendance.RollCall.global;
 
-public class DuplicateEmailException extends RuntimeException {
+public class DuplicateEmailException extends RuntimeException
+{
     public DuplicateEmailException(String message) {
         super(message);
     }
