3e3c0a7bffa24a20b7f21d914531499a8485e41a
김정우 <read2437@naver.com>
2025-11-12 00:34:14 +0900

    Member 서비스 로그인 구현
    findByEmail로 이메일 확인
    !passwordEncoder.matches(requestDto.password(), member.getPassword())
    로 비밀번호 일치 확인
    
    일치 시 인증토큰에 temp-jwt-token-for-user- + member.getID()하여 LoginResponseDto반환

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
@@ -1,15 +1,21 @@
 package com.example.LabAttendance.RollCall.Member;
 
+import com.example.LabAttendance.RollCall.Member.DTO.LoginRequestDto;
+import com.example.LabAttendance.RollCall.Member.DTO.LoginResponseDto;
 import com.example.LabAttendance.RollCall.Member.DTO.MemberResponseDto;
 import com.example.LabAttendance.RollCall.Member.DTO.MemberSignupRequestDto;
+import com.example.LabAttendance.RollCall.global.DuplicateEmailException;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.authentication.BadCredentialsException;
+import com.example.LabAttendance.RollCall.global.MemberNotFoundException;
 
 @RequiredArgsConstructor
 @Service
 @Transactional
+
 public class MemberService
 {
     private final MemberRepository memberRepository;
@@ -44,5 +50,32 @@ public class MemberService
                 member.getEmail()
         );
     }
+
+    public LoginResponseDto login(LoginRequestDto requestDto)
+    {
+
+        Member member = memberRepository.findByEmail(requestDto.email())
+                .orElseThrow(() -> new MemberNotFoundException("가입되지 않은 이메일입니다."));
+
+
+        if (!passwordEncoder.matches(requestDto.password(), member.getPassword())) {
+            throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
+        }
+
+        String generatedToken = "temp-jwt-token-for-user-" + member.getId();
+
+        return new LoginResponseDto(
+                // 인증 토큰 관련 필드 (임시 값)
+                generatedToken,    // accessToken
+                "Bearer",          // tokenType
+
+                // 사용자 정보 필드
+                member.getId(),
+                member.getNickname(),
+                member.getPhone(),
+                member.getEmail()
+        );
+    }
+
 
 }
