a203b97909e14e26320f168f2a22c6c1a9f7fa74
김정우 <read2437@naver.com>
2025-11-12 00:29:34 +0900

    LoginRequestDto 생성
    Login성공하면 응답하는 값 지정

---
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/DTO/LoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.LabAttendance.RollCall.Member.DTO;
+
+public record LoginResponseDto(
+        // 인증 토큰
+        String accessToken,
+        String tokenType,
+
+        Long memberId,
+        String username,
+        String phone,
+        String email
+
+){
+
+}
