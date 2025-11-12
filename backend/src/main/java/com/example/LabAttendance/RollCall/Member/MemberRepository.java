2c614acd1be18d85f22f6d548853e3f959ebb6c9
김정우 <read2437@naver.com>
2025-11-12 11:35:00 +0900

    MemberRepository 로직 구현
    findByEmail(), 중복 이메일 확인

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberRepository.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.example.LabAttendance.RollCall.Member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public interface MemberRepository extends JpaRepository<Member,Long>
 {
-
+    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
 
