bfd4a6211f71016a74ea9e235589b0a080aabf32
tomchccom <dreamkms2014@gmail.com>
2025-11-12 11:00:23 +0900

    Revert "Back/kimmoCya/23 (#25)"
    This reverts commit 62bf1d6245629c053534313c6ef34a2f45affc6a.

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.LabAttendance.RollCall.Member;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/MemberService.java
@@ -1,48 +1,14 @@
 package com.example.LabAttendance.RollCall.Member;
 
-import com.example.LabAttendance.RollCall.Member.DTO.MemberResponseDto;
-import com.example.LabAttendance.RollCall.Member.DTO.MemberSignupRequestDto;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
-@Transactional
 public class MemberService
 {
     private final MemberRepository memberRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public MemberResponseDto create(MemberSignupRequestDto requestDto)
-    {
-
-        if (memberRepository.existsByEmail(requestDto.email())) {
-            throw new DuplicateEmailException("이미 존재하는 이메일입니다: " + requestDto.email());
-        }
-
-        String rawPassword = requestDto.password();
-        String hashedPassword = passwordEncoder.encode(rawPassword);
 
 
-        Member member = Member.builder()
-                .memberId(requestDto.memberId())
-                .nickname(requestDto.nickname())
-                .password(hashedPassword)
-                .email(requestDto.email())
-                .phone(requestDto.phone())
-                .gender(requestDto.gender())
-                .build();
-
-        memberRepository.save(member);
-
-        return new MemberResponseDto(
-                member.getId(),     // Long 타입 ID
-                member.getMemberId(),
-                member.getNickname(),
-                member.getEmail()
-        );
-    }
 
 }
