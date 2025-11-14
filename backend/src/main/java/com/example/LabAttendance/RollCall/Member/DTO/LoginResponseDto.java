0e3a39cba0c5c44bba728312f3fef7bd4bd4f9d0
김정우 <read2437@naver.com>
2025-11-15 02:34:27 +0900

    LoginResponseDto fix
    불필요한 변수 삭제

--- backend/src/main/java/com/example/LabAttendance/RollCall/Member/DTO/LoginResponseDto.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/Member/DTO/LoginResponseDto.java
@@ -3,7 +3,6 @@ package com.example.LabAttendance.RollCall.Member.DTO;
 public record LoginResponseDto(
         // 인증 토큰
         String accessToken,
-        String tokenType,
 
         Long memberId,
         String username,
